6419992215cec5058fca2fe6514ff8ef8a227e8a
Jesper Persson <jespert.persson@gmail.com>
2013-09-24 16:59:27 +0200

    Update TournamentSelect to be able to handle tournaments of any size

--- intro_code_example/TournamentSelect.m
+++ intro_code_example/TournamentSelect.m
@@ -1,22 +1,29 @@
-function iSelected = TournamentSelect(fitness, tournamentSelectionParameter)
+function iSelected = TournamentSelect(fitness, tournamentSelectionParameter, tournamentSize)
+
 
 populationSize = size(fitness, 1);
-iTmp1 = 1 + fix(rand*populationSize);
-iTmp2 = 1 + fix(rand*populationSize);
 
-r = rand;
+% Preallocate for performance. Use a x*2-vector in order to be able to
+% store fitness-values for each individual - makes sorting easier.
+tournamentParticipants = zeros(tournamentSize, 2);      
 
-if(r < tournamentSelectionParameter)
-    if (fitness(iTmp1) > fitness(iTmp2))
-        iSelected = iTmp1;
-    else
-        iSelected = iTmp2;
-    end
-else
-    if (fitness(iTmp1) > fitness(iTmp2))
-        iSelected = iTmp2;
-    else
-        iSelected = iTmp1;
+for i = 1:tournamentSize
+    tournamentParticipants(i,1) = 1 + fix(rand*populationSize);
+    tournamentParticipants(i,2) = fitness(tournamentParticipants(i,1));
+end
+
+% Sort by fitness ascending.
+tournamentParticipants = sortrows(tournamentParticipants,-2);
+
+for i = 1:tournamentSize
+    r = rand;
+    if(r < tournamentSelectionParameter)
+        iSelected = tournamentParticipants(i,1);
+        return
     end
 end
 
+% If the loop ends, we know that the individual with the lowest fitness
+% value one, hence we return the last element from the sorted vector.
+iSelected = tournamentParticipants(tournamentSize, 1);
+end
