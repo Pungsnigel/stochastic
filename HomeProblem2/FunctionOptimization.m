c39efbb33e0983dba972d71ed782a657602f5845
Jesper Persson <jespert.persson@gmail.com>
2013-10-05 14:25:44 +0200

    Remove unused code for HomeProblem2

--- HomeProblem2/FunctionOptimization.m
+++ HomeProblem2/FunctionOptimization.m
@@ -1,52 +1,33 @@
+% ------------------- Global Variables ------------------- %
 populationSize                  = 100;
 numberOfGenes                   = 60;
-crossoverProbability            = 0.8;
 mutationProbability             = 0.025;
 tournamentSelectionParameter    = 0.75;
-variableRange                   = 5.0;
 numberOfGenerations             = 100;
 numberOfVariables               = 2;
 tournamentSize                  = 4;
-elitismDegree                   = 1;
 
 % Preallocate for performance.
 fitness = zeros(populationSize,1);
-
 population = InitializePopulation(populationSize, numberOfGenes);
 
 % -------------------- Main Algorithm -------------------- %
 
-for iGeneration = 1:numberOfGenerations
-    maximumFitness = 0.0; % Assumes non-negative fitness values!
-    
+for iGeneration = 1:numberOfGenerations    
     for i = 1:populationSize
         chromosome = population(i,:);
         x = DecodeChromosome(chromosome, numberOfVariables, variableRange);
         fitness(i) = EvaluateIndividualb(x);
-        if (fitness(i) > maximumFitness)
-            maximumFitness = fitness(i);
-            bestIndividualIndex = i;
-            xBest = x;
-        end
     end
 
     tempPopulation = population;
 
     for i = 1:2:populationSize
+        
         i1 = TournamentSelect(fitness,tournamentSelectionParameter, tournamentSize);
         i2 = TournamentSelect(fitness,tournamentSelectionParameter, tournamentSize);
-        chromosome1 = population(i1,:);
-        chromosome2 = population(i2,:);
-        
-        r = rand;
-        if (r < crossoverProbability)
-            newChromosomePair = Cross(chromosome1,chromosome2);
-            tempPopulation(i,:) = newChromosomePair(1,:);
-            tempPopulation(i+1,:) = newChromosomePair(2,:);
-        else
-            tempPopulation(i,:) = chromosome1;tempPopulation(i+1,:) = chromosome2;
-            tempPopulation(i+1,:) = chromosome2;
-        end
+        tempPopulation(i,:)   = population(i1,:);
+        tempPopulation(i+1,:) = population(i2,:);
     end % Loop over population
 
     for i = 1:populationSize
@@ -54,16 +35,9 @@ for iGeneration = 1:numberOfGenerations
         mutatedChromosome = Mutate(originalChromosome,mutationProbability);
         tempPopulation(i,:) = mutatedChromosome;
     end
-
-    % Carry out elitism and update population.
-    bestIndividual = population(bestIndividualIndex,:);
-    population = InsertBestIndividual(tempPopulation, bestIndividual, elitismDegree);
-
+    
+    % Update population to new generation.
+    population = tempPopulation;
+    
 end % Loop over generations
-
-% Print final result.
-% Since fitness value is just 1/function value, we can simply print
-% 1/fitness value to find the function value.
-fprintf('Found  min value %.3f at X1: %.3f, X2 %.3f \n',...
-1/maximumFitness, xBest(1), xBest(2));
     
