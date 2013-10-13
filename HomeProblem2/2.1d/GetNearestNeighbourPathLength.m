0b1c4a8b85d54764f0c4caf9fc6f2d1096df8ddb
Jesper Persson <jespert.persson@gmail.com>
2013-10-13 15:33:42 +0200

    Add Function for calculating NearestNeighbourBath to 2.1d

---
+++ HomeProblem2/2.1d/GetNearestNeighbourPathLength.m
@@ -0,0 +1,39 @@
+function [ length ] = GetNearestNeighbourPathLength( cityLocation )
+length = 0.0;
+nrOfCities = size(cityLocation,1);
+visited(1) = randi(nrOfCities);
+
+for iVisited = 1:nrOfCities-1 % Loop untill visited all cities
+    bestDistance = inf;
+    bestCityIndex = 0;
+    currentCity = visited(iVisited);
+    
+    for iCity = 1:nrOfCities% Find next city
+        if(ismember(iCity,visited))
+            continue
+        end
+        
+        % Calculate distance between current city and the one searched       
+        edge = [currentCity, iCity];
+        distance = GetEdgeLength(edge, cityLocation);
+                
+        % If we have not found a distance yet, or the distance is better
+        % than any earlier, update the bestDistance and bestCityIndex
+        % accordingly.
+        if (distance < bestDistance)
+            bestDistance = distance;
+            bestCityIndex = iCity;
+        end
+    end
+    % Add distance to total path length, and add city to visited.
+    length = length + bestDistance;    
+    visited(iVisited+1) = bestCityIndex;
+end
+
+%Finish by manuelly insert distance between last city and first
+
+path = [bestCityIndex, visited(1)];
+distance = GetPathLength(path, cityLocation);
+length = length + distance;
+
+end
